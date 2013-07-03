c10a5af566a2b40b47e5cc9508ad15423c5f9b4b
bangbang.S <bangbang.song@gmail.com>
2013-07-03 19:57:40 +0800

    add source .

---
+++ v4_viewpager_focus/src/com/example/viewpager/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.viewpager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private ViewPager mViewPager;
+    private  TextView mText;
+
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mViewPager = (ViewPager) findViewById(R.id.viewpager);
+		mViewPager.setAdapter(new PagerAdapter() {
+			
+			@Override
+			public boolean isViewFromObject(View arg0, Object arg1) {
+				// TODO Auto-generated method stub
+				return arg0 == arg1;
+			}
+			
+			@Override
+			public int getCount() {
+				return 5;
+			}
+			
+			
+			@Override
+			public Object instantiateItem(ViewGroup container, int position) {
+				View v = View.inflate(getApplicationContext(), R.layout.viewpager_content, null);
+                container.addView(v);
+				return v;
+			}
+			
+			@Override
+			public void destroyItem(ViewGroup container, int position,
+					Object object) {
+				container.removeView((View) object);
+			}
+		});
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                mText.setText(position + "/" + mViewPager.getAdapter().getCount());
+            }
+        });
+        mText = (TextView)findViewById(R.id.editText);
+		findViewById(R.id.button1).setOnClickListener(this);
+		findViewById(R.id.button2).setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		KeyEvent event = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT);
+		if (view.getId() == R.id.button1) {
+		} else {
+			event = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT);
+		}
+
+		mViewPager.executeKeyEvent(event);
+	}
+
+}
